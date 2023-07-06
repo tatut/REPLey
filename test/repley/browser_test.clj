ee6ce951e52a44a9fd1e56424dbdb750f38a48be
Tatu Tarvainen <tatu.tarvainen@me.com>
2023-07-06 13:15:28 +0300

    Breadcrumb navigation test

--- test/repley/browser_test.clj
+++ test/repley/browser_test.clj
@@ -32,23 +32,39 @@
     (is (= 101 (count customers))))
   (w/click (w/find-one-by-text :.tab "Table"))
   (w/wait "table.table")
-  (is (= 100 (w/count* (w/query "table.table tbody tr"))))
+  (is (= 20 (w/count* (w/query "table.table tbody tr"))))
   (w/fill [:evaluation "input"] "Fiji")
   (Thread/sleep 200)
   (is (= 1 (w/count* (w/query "table.table tbody tr")))))
 
-#_(defn breadcrumbs-test
+(defn breadcrumbs []
+  (.evaluate (w/get-page) "() => { var s = ''; document.querySelectorAll('div.breadcrumbs li').forEach(e=>s+=e.innerText+';'); return s; }"))
+
+(deftest breadcrumbs-test
   ;; evaluate a nested map structure
   (eval-in-repl "{:hello {:there {:my [\"friend\" {:name \"Rich\"} \"!\"]}}}")
-  ;; drill down from table
 
-  ;; drill back to :there
+  ;; descend deeper into result
+  (w/click (w/find-one-by-text "td" ":hello"))
+  (is (breadcrumbs) ";:hello")
+  (w/click (w/find-one-by-text "td" ":there"))
+  (is (breadcrumbs) ";:hello;:there")
+  (w/click (w/find-one-by-text "td" ":my"))
+  (is (breadcrumbs) ";:hello;:there;:my")
+  ;;(w/click "") ;; drill down from table
+  (w/click (w/find-one-by-text "span" "\"friend\""))
+  (is (breadcrumbs) ";:hello;:there;:my;0")
+
+  ;; drill back to :hello
+  (w/click (w/find-one-by-text "li" ":hello"))
+  (is (breadcrumbs) ";:hello")
 
   ;; drill back to home, breadcrumbs disappear
-  )
+  (w/click "div.breadcrumbs li:nth-child(1)")
+  (is (breadcrumbs) ""))
 
 (deftest table-data-escaped
   (eval-in-repl "{:foo 1 :bar \"<script>alert(2)</script>\"}")
   (w/click (w/find-one-by-text :.tab "Table"))
-  (.pause (w/get-page))
+  ;;(.pause (w/get-page))
   (is (w/find-one-by-text "td" "<script>alert(2)</script>")))
