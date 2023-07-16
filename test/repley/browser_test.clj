b571cbbf12475fc5d4f470472a90c7d1079f200a
Tatu Tarvainen <tatu.tarvainen@me.com>
2023-07-16 18:23:30 +0300

    Sleep a little before getting breadcrumbs

--- test/repley/browser_test.clj
+++ test/repley/browser_test.clj
@@ -38,6 +38,7 @@
   (is (= 1 (w/count* (w/query "table.table tbody tr")))))
 
 (defn breadcrumbs []
+  (Thread/sleep 100)
   (.evaluate (w/get-page) "() => { var s = ''; document.querySelectorAll('div.breadcrumbs li').forEach(e=>s+=e.innerText+';'); return s; }"))
 
 (deftest breadcrumbs-test
